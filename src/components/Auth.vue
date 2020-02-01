4d90d600ea14150629f78c277e5f0db0e61e01d0
Neboer <rubinposter@gmail.com>
2020-02-01 18:32:05 +0800

    前端优化

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,15 +1,17 @@
 <template>
     <el-container>
         <el-alert
-                title="设置了回调的 alert"
-                type="warning">
+                :title=alert
+                type="warning"
+                :style="{display: show_alert}"
+        >
         </el-alert>
         <el-header style="height: auto">
             <img style="max-height: 6rem" src="../assets/neu.jpg"/>
         </el-header>
-        <el-main>
-            <div class="el-col-10">
-                <el-form ref="form" :model="form" label-width="80px" @submit="">
+        <el-main style="text-align: center">
+            <div style="margin: auto; width: 40rem">
+                <el-form ref="form" :model="form" label-width="80px">
                     <el-form-item label="用户名">
                         <el-input v-model="form.username"></el-input>
                     </el-form-item>
@@ -21,11 +23,11 @@
                             <el-input v-model="form.captcha"></el-input>
                         </el-col>
                         <el-col :span="11">
-                            <img :src="captcha_url" @click="refresh_captcha">
+                            <img :src="captcha_url" @click="refresh_captcha()">
                         </el-col>
                     </el-form-item>
                     <el-form-item>
-                        <el-button type="primary">登录</el-button>
+                        <el-button type="primary" @click="submit()">登录</el-button>
                         <el-button>使用统一身份认证</el-button>
                     </el-form-item>
                 </el-form>
@@ -44,7 +46,9 @@
                     password: '',
                     captcha: ''
                 },
-                captcha_url: "/auth/captcha"
+                captcha_url: "/auth/captcha",
+                show_alert: false,
+                alert: "密码错误。"
             }
         },
         props: {
@@ -57,17 +61,18 @@
             },
             submit() {
                 this.axios.post("/auth/login", this.form).then((code) => {
-                    if (code == 0){
+                    if (code == 0) {
                         // 登录成功，准予跳转。
                     } else {
-                        if(code == 1){
-                            this.$alert('验证码错误', '错误')
-                        }else if (code == 4){
-                            this.$alert('用户已被禁止登录','错误')
-                        }else {
-                            this.$alert('用户名或密码错误','错误')
+                        if (code == 1) {
+                            this.alert = '验证码错误'
+                        } else if (code == 4) {
+                            this.alert = '用户已被禁止登录'
+                        } else {
+                            this.alert = '用户名或密码错误'
                         }
                     }
+                    this.show_alert = true
                 })
             }
         }
